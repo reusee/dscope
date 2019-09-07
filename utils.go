42cbb6fc78f8fb5f32f19b0b418afc095bbd7184
reus <reusee@gmail.com>
2019-09-07 13:35:25 +0800

    make Methods variadic

--- utils.go
+++ utils.go
@@ -9,10 +9,12 @@ var (
 	pt = fmt.Printf
 )
 
-func Methods(o interface{}) (ret []interface{}) {
-	v := reflect.ValueOf(o)
-	for i := 0; i < v.NumMethod(); i++ {
-		ret = append(ret, v.Method(i).Interface())
+func Methods(objects ...interface{}) (ret []interface{}) {
+	for _, object := range objects {
+		v := reflect.ValueOf(object)
+		for i := 0; i < v.NumMethod(); i++ {
+			ret = append(ret, v.Method(i).Interface())
+		}
 	}
 	return
 }
