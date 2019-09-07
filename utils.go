43008ab8fb6ac3ae7b0794f668ea612c09d89bbb
reus <reusee@gmail.com>
2019-09-07 12:51:06 +0800

    add Methods util func

--- utils.go
+++ utils.go
@@ -2,8 +2,17 @@ package dscope
 
 import (
 	"fmt"
+	"reflect"
 )
 
 var (
 	pt = fmt.Printf
 )
+
+func Methods(o interface{}) (ret []interface{}) {
+	v := reflect.ValueOf(o)
+	for i := 0; i < v.NumMethod(); i++ {
+		ret = append(ret, v.Method(i))
+	}
+	return
+}
