0834a7e393d7dffce7a1cde1ac4f6bdfb9461d47
reus <reusee@gmail.com>
2021-12-26 03:32:57 +0800

    add Defs.Copy; add defs param to Defs.NewScope

--- defs.go
+++ defs.go
@@ -6,6 +6,13 @@ func (d *Defs) Add(defs ...any) {
 	*d = append(*d, defs...)
 }
 
-func (d Defs) NewScope() Scope {
-	return New(d...)
+func (d Defs) Copy() []any {
+	ret := make([]any, len(d))
+	copy(ret, d)
+	return ret
+}
+
+func (d Defs) NewScope(defs ...any) Scope {
+	defs = append(d.Copy(), defs...)
+	return New(defs...)
 }
--- defs_test.go
+++ defs_test.go
@@ -3,16 +3,16 @@ package dscope
 import "testing"
 
 func TestDefs(t *testing.T) {
-	defs := Defs{
-		func() int {
-			return 42
-		},
-	}
+	defs := Defs{}
 	defs.Add(func(i int) int8 {
 		return int8(i % 256)
 	})
 	var i8 int8
-	defs.NewScope().Assign(&i8)
+	defs.NewScope(
+		func() int {
+			return 42
+		},
+	).Assign(&i8)
 	if i8 != 42 {
 		t.Fatal()
 	}
