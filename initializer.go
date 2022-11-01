f9ad0e7a1e73e453d856cc5784663cfae4fc8146
reus <reusee@gmail.com>
2022-11-01 12:46:30 +0800

    optimize _Initializer.get

--- initializer.go
+++ initializer.go
@@ -13,8 +13,8 @@ type _Initializer struct {
 	Def         any
 	ReducerType *reflect.Type
 	Values      []reflect.Value
-	Once        sync.Once
-	done        atomic.Bool
+	done        uint32
+	mu          sync.Mutex
 	err         error
 }
 
@@ -29,7 +29,8 @@ func newInitializer(def any, reducerType *reflect.Type) *_Initializer {
 var nextInitializerID int64 = 42
 
 func (i *_Initializer) get(scope Scope) (ret []reflect.Value, err error) {
-	if i.done.Load() {
+	if atomic.LoadUint32(&i.done) == 1 {
+		// init done
 		return i.Values, i.err
 	}
 	return i.getSlow(scope)
@@ -50,63 +51,67 @@ func (i *_Initializer) getSlow(scope Scope) (ret []reflect.Value, err error) {
 		)
 	}
 
-	i.Once.Do(func() {
-		defer func() {
-			i.Values = ret
-			i.err = err
-			i.done.Store(true)
-		}()
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	if i.done == 1 {
+		// init done
+		return i.Values, i.err
+	}
 
-		// reducer
-		if i.ReducerType != nil {
-			typ := i.Def.(reflect.Type)
-			typeID := getTypeID(typ)
-			values, ok := scope.values.Load(typeID)
-			if !ok { // NOCOVER
-				panic("impossible")
-			}
-			pathScope := scope.appendPath(typeID)
-			vs := make([]reflect.Value, len(values))
-			for i, value := range values {
-				var values []reflect.Value
-				values, err = value.initializer.get(pathScope)
-				if err != nil { // NOCOVER
-					return
-				}
-				vs[i] = values[value.typeInfo.Position]
-			}
-			switch *i.ReducerType {
-			case reducerType:
-				ret = []reflect.Value{
-					Reduce(vs),
-				}
-			case customReducerType:
-				ret = []reflect.Value{
-					vs[0].Interface().(CustomReducer).Reduce(scope, vs),
-				}
-			}
-			return
+	defer func() {
+		i.Values = ret
+		i.err = err
+		atomic.StoreUint32(&i.done, 1)
+	}()
+
+	// reducer
+	if i.ReducerType != nil {
+		typ := i.Def.(reflect.Type)
+		typeID := getTypeID(typ)
+		values, ok := scope.values.Load(typeID)
+		if !ok { // NOCOVER
+			panic("impossible")
 		}
-
-		// non-reducer
-		defValue := reflect.ValueOf(i.Def)
-		defKind := defValue.Kind()
-
-		switch defKind {
-
-		case reflect.Func:
-			var result CallResult
-			result = scope.Call(i.Def)
-			ret = result.Values
-
-		case reflect.Ptr:
-			ret = []reflect.Value{defValue.Elem()}
-
+		pathScope := scope.appendPath(typeID)
+		vs := make([]reflect.Value, len(values))
+		for i, value := range values {
+			var values []reflect.Value
+			values, err = value.initializer.get(pathScope)
+			if err != nil { // NOCOVER
+				return
+			}
+			vs[i] = values[value.typeInfo.Position]
 		}
+		switch *i.ReducerType {
+		case reducerType:
+			ret = []reflect.Value{
+				Reduce(vs),
+			}
+		case customReducerType:
+			ret = []reflect.Value{
+				vs[0].Interface().(CustomReducer).Reduce(scope, vs),
+			}
+		}
+		return ret, nil
+	}
 
-	})
+	// non-reducer
+	defValue := reflect.ValueOf(i.Def)
+	defKind := defValue.Kind()
 
-	return i.Values, i.err
+	switch defKind {
+
+	case reflect.Func:
+		var result CallResult
+		result = scope.Call(i.Def)
+		ret = result.Values
+
+	case reflect.Ptr:
+		ret = []reflect.Value{defValue.Elem()}
+
+	}
+
+	return ret, nil
 }
 
 func (s *_Initializer) reset() *_Initializer {
