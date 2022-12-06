7e0457f211a44fb86a2a3b0f576f3d4f846a4b88
reus <reusee@gmail.com>
2022-12-06 16:37:31 +0800

    add WithFork3~8

--- with_fork.go
+++ with_fork.go
@@ -41,6 +41,168 @@ func (w WithFork2[T, T2]) unwrapType() []reflect.Type {
 	}
 }
 
+type WithFork3[T any, T2 any, T3 any] func(defs ...any) (T, T2, T3)
+
+func (w WithFork3[T, T2, T3]) newWithForkValue(scope Scope) reflect.Value {
+	return reflect.ValueOf(WithFork3[T, T2, T3](func(defs ...any) (t T, t2 T2, t3 T3) {
+		scope.Fork(defs...).Assign(&t, &t2, &t3)
+		return
+	}))
+}
+
+var _ typeWrapper = WithFork3[int, int, int](nil)
+
+func (w WithFork3[T, T2, T3]) unwrapType() []reflect.Type {
+	var t T
+	var t2 T2
+	var t3 T3
+	return []reflect.Type{
+		reflect.TypeOf(t),
+		reflect.TypeOf(t2),
+		reflect.TypeOf(t3),
+	}
+}
+
+type WithFork4[T any, T2 any, T3 any, T4 any] func(defs ...any) (T, T2, T3, T4)
+
+func (w WithFork4[T, T2, T3, T4]) newWithForkValue(scope Scope) reflect.Value {
+	return reflect.ValueOf(WithFork4[T, T2, T3, T4](func(defs ...any) (t T, t2 T2, t3 T3, t4 T4) {
+		scope.Fork(defs...).Assign(&t, &t2, &t3, &t4)
+		return
+	}))
+}
+
+var _ typeWrapper = WithFork4[int, int, int, int](nil)
+
+func (w WithFork4[T, T2, T3, T4]) unwrapType() []reflect.Type {
+	var t T
+	var t2 T2
+	var t3 T3
+	var t4 T4
+	return []reflect.Type{
+		reflect.TypeOf(t),
+		reflect.TypeOf(t2),
+		reflect.TypeOf(t3),
+		reflect.TypeOf(t4),
+	}
+}
+
+type WithFork5[T any, T2 any, T3 any, T4 any, T5 any] func(defs ...any) (T, T2, T3, T4, T5)
+
+func (w WithFork5[T, T2, T3, T4, T5]) newWithForkValue(scope Scope) reflect.Value {
+	return reflect.ValueOf(WithFork5[T, T2, T3, T4, T5](func(defs ...any) (t T, t2 T2, t3 T3, t4 T4, t5 T5) {
+		scope.Fork(defs...).Assign(&t, &t2, &t3, &t4, &t5)
+		return
+	}))
+}
+
+var _ typeWrapper = WithFork5[int, int, int, int, int](nil)
+
+func (w WithFork5[T, T2, T3, T4, T5]) unwrapType() []reflect.Type {
+	var t T
+	var t2 T2
+	var t3 T3
+	var t4 T4
+	var t5 T5
+	return []reflect.Type{
+		reflect.TypeOf(t),
+		reflect.TypeOf(t2),
+		reflect.TypeOf(t3),
+		reflect.TypeOf(t4),
+		reflect.TypeOf(t5),
+	}
+}
+
+type WithFork6[T any, T2 any, T3 any, T4 any, T5 any, T6 any] func(defs ...any) (T, T2, T3, T4, T5, T6)
+
+func (w WithFork6[T, T2, T3, T4, T5, T6]) newWithForkValue(scope Scope) reflect.Value {
+	return reflect.ValueOf(WithFork6[T, T2, T3, T4, T5, T6](func(defs ...any) (t T, t2 T2, t3 T3, t4 T4, t5 T5, t6 T6) {
+		scope.Fork(defs...).Assign(&t, &t2, &t3, &t4, &t5, &t6)
+		return
+	}))
+}
+
+var _ typeWrapper = WithFork6[int, int, int, int, int, int](nil)
+
+func (w WithFork6[T, T2, T3, T4, T5, T6]) unwrapType() []reflect.Type {
+	var t T
+	var t2 T2
+	var t3 T3
+	var t4 T4
+	var t5 T5
+	var t6 T6
+	return []reflect.Type{
+		reflect.TypeOf(t),
+		reflect.TypeOf(t2),
+		reflect.TypeOf(t3),
+		reflect.TypeOf(t4),
+		reflect.TypeOf(t5),
+		reflect.TypeOf(t6),
+	}
+}
+
+type WithFork7[T any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any] func(defs ...any) (T, T2, T3, T4, T5, T6, T7)
+
+func (w WithFork7[T, T2, T3, T4, T5, T6, T7]) newWithForkValue(scope Scope) reflect.Value {
+	return reflect.ValueOf(WithFork7[T, T2, T3, T4, T5, T6, T7](func(defs ...any) (t T, t2 T2, t3 T3, t4 T4, t5 T5, t6 T6, t7 T7) {
+		scope.Fork(defs...).Assign(&t, &t2, &t3, &t4, &t5, &t6, &t7)
+		return
+	}))
+}
+
+var _ typeWrapper = WithFork7[int, int, int, int, int, int, int](nil)
+
+func (w WithFork7[T, T2, T3, T4, T5, T6, T7]) unwrapType() []reflect.Type {
+	var t T
+	var t2 T2
+	var t3 T3
+	var t4 T4
+	var t5 T5
+	var t6 T6
+	var t7 T7
+	return []reflect.Type{
+		reflect.TypeOf(t),
+		reflect.TypeOf(t2),
+		reflect.TypeOf(t3),
+		reflect.TypeOf(t4),
+		reflect.TypeOf(t5),
+		reflect.TypeOf(t6),
+		reflect.TypeOf(t7),
+	}
+}
+
+type WithFork8[T any, T2 any, T3 any, T4 any, T5 any, T6 any, T7 any, T8 any] func(defs ...any) (T, T2, T3, T4, T5, T6, T7, T8)
+
+func (w WithFork8[T, T2, T3, T4, T5, T6, T7, T8]) newWithForkValue(scope Scope) reflect.Value {
+	return reflect.ValueOf(WithFork8[T, T2, T3, T4, T5, T6, T7, T8](func(defs ...any) (t T, t2 T2, t3 T3, t4 T4, t5 T5, t6 T6, t7 T7, t8 T8) {
+		scope.Fork(defs...).Assign(&t, &t2, &t3, &t4, &t5, &t6, &t7, &t8)
+		return
+	}))
+}
+
+var _ typeWrapper = WithFork8[int, int, int, int, int, int, int, int](nil)
+
+func (w WithFork8[T, T2, T3, T4, T5, T6, T7, T8]) unwrapType() []reflect.Type {
+	var t T
+	var t2 T2
+	var t3 T3
+	var t4 T4
+	var t5 T5
+	var t6 T6
+	var t7 T7
+	var t8 T8
+	return []reflect.Type{
+		reflect.TypeOf(t),
+		reflect.TypeOf(t2),
+		reflect.TypeOf(t3),
+		reflect.TypeOf(t4),
+		reflect.TypeOf(t5),
+		reflect.TypeOf(t6),
+		reflect.TypeOf(t7),
+		reflect.TypeOf(t8),
+	}
+}
+
 type withFork interface {
 	newWithForkValue(scope Scope) reflect.Value
 }
--- with_fork_test.go
+++ with_fork_test.go
@@ -55,14 +55,399 @@ func TestWithForkN(t *testing.T) {
 		func(i int) int16 {
 			return int16(i * 2)
 		},
+		func(i int) int32 {
+			return int32(i * 3)
+		},
+		func(i int) int64 {
+			return int64(i * 4)
+		},
+		func(i int) uint8 {
+			return uint8(i * 5)
+		},
+		func(i int) uint16 {
+			return uint16(i * 6)
+		},
+		func(i int) uint32 {
+			return uint32(i * 7)
+		},
+		func(i int) uint64 {
+			return uint64(i * 8)
+		},
 	)
-	var with WithFork2[int8, int16]
-	s.Assign(&with)
-	i1, i2 := with(PtrTo(1))
-	if i1 != 1 {
-		t.Fatal()
+
+	{
+		var with WithFork2[int8, int16]
+		s.Assign(&with)
+		i1, i2 := with(PtrTo(1))
+		if i1 != 1 {
+			t.Fatal()
+		}
+		if i2 != 2 {
+			t.Fatal()
+		}
 	}
-	if i2 != 2 {
-		t.Fatal()
+
+	{
+		var with WithFork3[int8, int16, int32]
+		s.Assign(&with)
+		i1, i2, i3 := with(PtrTo(1))
+		if i1 != 1 {
+			t.Fatal()
+		}
+		if i2 != 2 {
+			t.Fatal()
+		}
+		if i3 != 3 {
+			t.Fatal()
+		}
 	}
+
+	{
+		var with WithFork4[int8, int16, int32, int64]
+		s.Assign(&with)
+		i1, i2, i3, i4 := with(PtrTo(1))
+		if i1 != 1 {
+			t.Fatal()
+		}
+		if i2 != 2 {
+			t.Fatal()
+		}
+		if i3 != 3 {
+			t.Fatal()
+		}
+		if i4 != 4 {
+			t.Fatal()
+		}
+	}
+
+	{
+		var with WithFork5[int8, int16, int32, int64, uint8]
+		s.Assign(&with)
+		i1, i2, i3, i4, i5 := with(PtrTo(1))
+		if i1 != 1 {
+			t.Fatal()
+		}
+		if i2 != 2 {
+			t.Fatal()
+		}
+		if i3 != 3 {
+			t.Fatal()
+		}
+		if i4 != 4 {
+			t.Fatal()
+		}
+		if i5 != 5 {
+			t.Fatal()
+		}
+	}
+
+	{
+		var with WithFork6[int8, int16, int32, int64, uint8, uint16]
+		s.Assign(&with)
+		i1, i2, i3, i4, i5, i6 := with(PtrTo(1))
+		if i1 != 1 {
+			t.Fatal()
+		}
+		if i2 != 2 {
+			t.Fatal()
+		}
+		if i3 != 3 {
+			t.Fatal()
+		}
+		if i4 != 4 {
+			t.Fatal()
+		}
+		if i5 != 5 {
+			t.Fatal()
+		}
+		if i6 != 6 {
+			t.Fatal()
+		}
+	}
+
+	{
+		var with WithFork7[int8, int16, int32, int64, uint8, uint16, uint32]
+		s.Assign(&with)
+		i1, i2, i3, i4, i5, i6, i7 := with(PtrTo(1))
+		if i1 != 1 {
+			t.Fatal()
+		}
+		if i2 != 2 {
+			t.Fatal()
+		}
+		if i3 != 3 {
+			t.Fatal()
+		}
+		if i4 != 4 {
+			t.Fatal()
+		}
+		if i5 != 5 {
+			t.Fatal()
+		}
+		if i6 != 6 {
+			t.Fatal()
+		}
+		if i7 != 7 {
+			t.Fatal()
+		}
+	}
+
+	{
+		var with WithFork8[int8, int16, int32, int64, uint8, uint16, uint32, uint64]
+		s.Assign(&with)
+		i1, i2, i3, i4, i5, i6, i7, i8 := with(PtrTo(1))
+		if i1 != 1 {
+			t.Fatal()
+		}
+		if i2 != 2 {
+			t.Fatal()
+		}
+		if i3 != 3 {
+			t.Fatal()
+		}
+		if i4 != 4 {
+			t.Fatal()
+		}
+		if i5 != 5 {
+			t.Fatal()
+		}
+		if i6 != 6 {
+			t.Fatal()
+		}
+		if i7 != 7 {
+			t.Fatal()
+		}
+		if i8 != 8 {
+			t.Fatal()
+		}
+	}
+
+	{
+		s = s.Fork(
+			func(
+				with WithFork2[
+					int8, int16,
+				],
+			) func() {
+				return func() {
+					i1, i2 := with(PtrTo(1))
+					if i1 != 1 {
+						t.Fatal()
+					}
+					if i2 != 2 {
+						t.Fatal()
+					}
+				}
+			},
+		)
+		var fn func()
+		s.Assign(&fn)
+		fn()
+	}
+
+	{
+		s = s.Fork(
+			func(
+				with WithFork3[
+					int8, int16, int32,
+				],
+			) func() {
+				return func() {
+					i1, i2, i3 := with(PtrTo(1))
+					if i1 != 1 {
+						t.Fatal()
+					}
+					if i2 != 2 {
+						t.Fatal()
+					}
+					if i3 != 3 {
+						t.Fatal()
+					}
+				}
+			},
+		)
+		var fn func()
+		s.Assign(&fn)
+		fn()
+	}
+
+	{
+		s = s.Fork(
+			func(
+				with WithFork4[
+					int8, int16, int32, int64,
+				],
+			) func() {
+				return func() {
+					i1, i2, i3, i4 := with(PtrTo(1))
+					if i1 != 1 {
+						t.Fatal()
+					}
+					if i2 != 2 {
+						t.Fatal()
+					}
+					if i3 != 3 {
+						t.Fatal()
+					}
+					if i4 != 4 {
+						t.Fatal()
+					}
+				}
+			},
+		)
+		var fn func()
+		s.Assign(&fn)
+		fn()
+	}
+
+	{
+		s = s.Fork(
+			func(
+				with WithFork5[
+					int8, int16, int32, int64,
+					uint8,
+				],
+			) func() {
+				return func() {
+					i1, i2, i3, i4, i5 := with(PtrTo(1))
+					if i1 != 1 {
+						t.Fatal()
+					}
+					if i2 != 2 {
+						t.Fatal()
+					}
+					if i3 != 3 {
+						t.Fatal()
+					}
+					if i4 != 4 {
+						t.Fatal()
+					}
+					if i5 != 5 {
+						t.Fatal()
+					}
+				}
+			},
+		)
+		var fn func()
+		s.Assign(&fn)
+		fn()
+	}
+
+	{
+		s = s.Fork(
+			func(
+				with WithFork6[
+					int8, int16, int32, int64,
+					uint8, uint16,
+				],
+			) func() {
+				return func() {
+					i1, i2, i3, i4, i5, i6 := with(PtrTo(1))
+					if i1 != 1 {
+						t.Fatal()
+					}
+					if i2 != 2 {
+						t.Fatal()
+					}
+					if i3 != 3 {
+						t.Fatal()
+					}
+					if i4 != 4 {
+						t.Fatal()
+					}
+					if i5 != 5 {
+						t.Fatal()
+					}
+					if i6 != 6 {
+						t.Fatal()
+					}
+				}
+			},
+		)
+		var fn func()
+		s.Assign(&fn)
+		fn()
+	}
+
+	{
+		s = s.Fork(
+			func(
+				with WithFork7[
+					int8, int16, int32, int64,
+					uint8, uint16, uint32,
+				],
+			) func() {
+				return func() {
+					i1, i2, i3, i4, i5, i6, i7 := with(PtrTo(1))
+					if i1 != 1 {
+						t.Fatal()
+					}
+					if i2 != 2 {
+						t.Fatal()
+					}
+					if i3 != 3 {
+						t.Fatal()
+					}
+					if i4 != 4 {
+						t.Fatal()
+					}
+					if i5 != 5 {
+						t.Fatal()
+					}
+					if i6 != 6 {
+						t.Fatal()
+					}
+					if i7 != 7 {
+						t.Fatal()
+					}
+				}
+			},
+		)
+		var fn func()
+		s.Assign(&fn)
+		fn()
+	}
+
+	{
+		s = s.Fork(
+			func(
+				with WithFork8[
+					int8, int16, int32, int64,
+					uint8, uint16, uint32, uint64,
+				],
+			) func() {
+				return func() {
+					i1, i2, i3, i4, i5, i6, i7, i8 := with(PtrTo(1))
+					if i1 != 1 {
+						t.Fatal()
+					}
+					if i2 != 2 {
+						t.Fatal()
+					}
+					if i3 != 3 {
+						t.Fatal()
+					}
+					if i4 != 4 {
+						t.Fatal()
+					}
+					if i5 != 5 {
+						t.Fatal()
+					}
+					if i6 != 6 {
+						t.Fatal()
+					}
+					if i7 != 7 {
+						t.Fatal()
+					}
+					if i8 != 8 {
+						t.Fatal()
+					}
+				}
+			},
+		)
+		var fn func()
+		s.Assign(&fn)
+		fn()
+	}
+
 }
