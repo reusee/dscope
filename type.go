679f350bb08b1a9f96fa8ccf06163e2878858c05
reus <reusee@gmail.com>
2023-03-31 23:52:49 +0800

    Revert "use swiss map"
    This reverts commit 41cd4c39c0c36fe864c5438f9632bc7c3cb28ed5.

--- type.go
+++ type.go
@@ -4,20 +4,18 @@ import (
 	"reflect"
 	"sync"
 	"sync/atomic"
-
-	"github.com/dolthub/swiss"
 )
 
 type _TypeIDInfos struct {
 	TypeToID map[reflect.Type]_TypeID
-	IDToType *swiss.Map[_TypeID, reflect.Type]
+	IDToType map[_TypeID]reflect.Type
 }
 
 var (
 	typeIDInfos = func() *atomic.Pointer[_TypeIDInfos] {
 		infos := &_TypeIDInfos{
 			TypeToID: make(map[reflect.Type]_TypeID),
-			IDToType: swiss.NewMap[_TypeID, reflect.Type](0),
+			IDToType: make(map[_TypeID]reflect.Type),
 		}
 		v := new(atomic.Pointer[_TypeIDInfos])
 		v.Store(infos)
@@ -46,15 +44,14 @@ func getTypeIDSlow(t reflect.Type) _TypeID {
 	for k, v := range infos.TypeToID {
 		newTypeToID[k] = v
 	}
-	newIDToType := swiss.NewMap[_TypeID, reflect.Type](uint32(infos.IDToType.Count() + 1))
-	infos.IDToType.Iter(func(k _TypeID, v reflect.Type) bool {
-		newIDToType.Put(k, v)
-		return false
-	})
+	newIDToType := make(map[_TypeID]reflect.Type, len(infos.IDToType)+1)
+	for k, v := range infos.IDToType {
+		newIDToType[k] = v
+	}
 	nextTypeID++
 	id := _TypeID(nextTypeID)
 	newTypeToID[t] = id
-	newIDToType.Put(id, t)
+	newIDToType[id] = t
 	typeIDInfos.Store(&_TypeIDInfos{
 		TypeToID: newTypeToID,
 		IDToType: newIDToType,
@@ -63,7 +60,6 @@ func getTypeIDSlow(t reflect.Type) _TypeID {
 	return id
 }
 
-func typeIDToType(id _TypeID) (t reflect.Type) {
-	t, _ = typeIDInfos.Load().IDToType.Get(id)
-	return
+func typeIDToType(id _TypeID) reflect.Type {
+	return typeIDInfos.Load().IDToType[id]
 }
